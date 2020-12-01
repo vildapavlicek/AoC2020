2acb57bbe4014957fed73882b3b0160aa354b85a
Vilém Pavlíček <pavlicek@cngroup.dk>
2020-12-01 10:35:54 +0100

    Revert "first day solution"
    This reverts commit 8cfcc954e26e8520e1b1f32d1f4d53c7650ceaec.

--- 01_report_repair/target/debug/deps/report_repair.d
+++
@@ -1,5 +0,0 @@
-C:\Users\pavlicek\Documents\Projects\Rust\aoc_2020\01_report_repair\target\debug\deps\report_repair.exe: src\main.rs
-
-C:\Users\pavlicek\Documents\Projects\Rust\aoc_2020\01_report_repair\target\debug\deps\report_repair.d: src\main.rs
-
-src\main.rs:
--- 01_report_repair/target/debug/report_repair.d
+++
@@ -1,1 +0,0 @@
-C:\Users\pavlicek\Documents\Projects\Rust\aoc_2020\01_report_repair\target\debug\report_repair.exe: C:\Users\pavlicek\Documents\Projects\Rust\aoc_2020\01_report_repair\src\main.rs
