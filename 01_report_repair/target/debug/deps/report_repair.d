8cfcc954e26e8520e1b1f32d1f4d53c7650ceaec
Vilém Pavlíček <pavlicek@cngroup.dk>
2020-12-01 10:35:00 +0100

    first day solution

---
+++ 01_report_repair/target/debug/deps/report_repair.d
@@ -0,0 +1,5 @@
+C:\Users\pavlicek\Documents\Projects\Rust\aoc_2020\01_report_repair\target\debug\deps\report_repair.exe: src\main.rs
+
+C:\Users\pavlicek\Documents\Projects\Rust\aoc_2020\01_report_repair\target\debug\deps\report_repair.d: src\main.rs
+
+src\main.rs:
---
+++ 01_report_repair/target/debug/report_repair.d
@@ -0,0 +1,1 @@
+C:\Users\pavlicek\Documents\Projects\Rust\aoc_2020\01_report_repair\target\debug\report_repair.exe: C:\Users\pavlicek\Documents\Projects\Rust\aoc_2020\01_report_repair\src\main.rs
