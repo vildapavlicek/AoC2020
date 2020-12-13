4ba16a592d35b6e23a44dc7528c4353a93818f55
Vilém Pavlíček <pavlicek@cngroup.dk>
2020-12-13 13:54:15 +0100

    formating

--- 05_binary_boarding/src/boarding_pass.rs
+++ 05_binary_boarding/src/boarding_pass.rs
@@ -2,7 +2,9 @@ pub struct BoardingPasses(String);
 
 impl BoardingPasses {
     pub fn load() -> Result<Self, Box<dyn std::error::Error>> {
-        Ok(BoardingPasses(std::fs::read_to_string("./05_binary_boarding/input.txt")?))
+        Ok(BoardingPasses(std::fs::read_to_string(
+            "./05_binary_boarding/input.txt",
+        )?))
     }
 
     pub fn find_highest_id(&self) -> i64 {
@@ -19,15 +21,28 @@ impl BoardingPasses {
         let mut my_seat: i64 = 0;
 
         loop {
-            if ids.contains(&(my_seat + 1)) && ids.contains(&(my_seat - 1)) && !ids.contains(&my_seat) { break; } else { my_seat += 1};
-        };
-            my_seat
+            if ids.contains(&(my_seat + 1))
+                && ids.contains(&(my_seat - 1))
+                && !ids.contains(&my_seat)
+            {
+                break;
+            } else {
+                my_seat += 1
+            };
+        }
+        my_seat
     }
 
     fn get_ids(&self) -> Vec<i64> {
-        self.0.lines().map(|line| {
-            let s: String = line.chars().map(|c|  { if c == 'B' || c == 'R' {'1'} else {'0'}}).collect();
-            i64::from_str_radix(s.as_str(), 2).unwrap()
-        }).collect::<Vec<i64>>()
+        self.0
+            .lines()
+            .map(|line| {
+                let s: String = line
+                    .chars()
+                    .map(|c| if c == 'B' || c == 'R' { '1' } else { '0' })
+                    .collect();
+                i64::from_str_radix(s.as_str(), 2).unwrap()
+            })
+            .collect::<Vec<i64>>()
     }
 }
--- 06_custom_customs/src/main.rs
+++ 06_custom_customs/src/main.rs
@@ -2,5 +2,7 @@ mod customs;
 
 fn main() {
     println!("Hello, world!");
-    customs::Answers::load().expect("failed to load input file").count_answers();
+    customs::Answers::load()
+        .expect("failed to load input file")
+        .count_answers();
 }
